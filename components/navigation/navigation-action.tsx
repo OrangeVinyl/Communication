e25928037ba21c97bdfa538fa871bce660bf26bc
Su Hwan CHOI <hap_hazard@naver.com>
2023-12-15 23:28:42 +0900

    ✨feat: navigation sidebar 기능 추가 #2
    - 새로운 서버 생성에 관한 모달 컴포넌트 추가
    - zustand를 통한 모달 hooks의 상태관리
    - modal의 provider를 생성해 상태 변경

--- components/navigation/navigation-action.tsx
+++ components/navigation/navigation-action.tsx
@@ -1,13 +1,19 @@
 'use client';
 
 import { Plus } from 'lucide-react';
+
+// components
 import { ActionTooltip } from '@/components/action-tooltip';
 
+// hooks
+import { useModal } from '@/hooks/use-modal-store';
+
 export const NavigationAction = () => {
+  const { onOpen } = useModal();
   return (
     <div>
       <ActionTooltip side={'right'} align={'center'} label={'💡서버를 추가하세요!'}>
-        <button className={'group flex items-center'}>
+        <button className={'group flex items-center'} onClick={() => onOpen('createServer')}>
           <div
             className={
               'flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden items-center justify-center bg-background dark:bg-neutral-700 group-hover:bg-emerald-500'
