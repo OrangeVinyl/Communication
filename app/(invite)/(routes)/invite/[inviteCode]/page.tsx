fd4dc40664e8be473d0e1b3cef249286a2f7ee44
Su Hwan CHOI <hap_hazard@naver.com>
2023-12-20 00:38:48 +0900

    ✨feat: 초대 기능 구현

---
+++ app/(invite)/(routes)/invite/[inviteCode]/page.tsx
@@ -0,0 +1,61 @@
+import { redirect } from 'next/navigation';
+import { currentProfile } from '@/lib/current-profile';
+import { redirectToSignIn } from '@clerk/nextjs';
+
+import { db } from '@/lib/db';
+
+/**
+ * @description 서버 존재 유무에 따른 route 역할을 하는 page
+ */
+
+interface InviteCodePageProps {
+  params: {
+    inviteCode: string;
+  };
+}
+
+const InviteCodePage = async ({ params }: InviteCodePageProps) => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirectToSignIn();
+  }
+
+  if (!params.inviteCode) {
+    return redirect('/');
+  }
+
+  const existingServer = await db.server.findFirst({
+    where: {
+      inviteCode: params.inviteCode,
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (existingServer) {
+    return redirect(`/servers/${existingServer.id}`);
+  }
+
+  const server = await db.server.update({
+    where: {
+      inviteCode: params.inviteCode,
+    },
+    data: {
+      members: {
+        create: [{ profileId: profile.id }],
+      },
+    },
+  });
+
+  if (server) {
+    return redirect(`/servers/${server.id}`);
+  }
+
+  return null;
+};
+
+export default InviteCodePage;
