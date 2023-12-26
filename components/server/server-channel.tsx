338cd20089b48920944a83e6883272bf6fe24f60
Su Hwan CHOI <hap_hazard@naver.com>
2023-12-26 22:02:34 +0900

    🚨fix: redirection 관련 오류 수정

--- components/server/server-channel.tsx
+++ components/server/server-channel.tsx
@@ -8,7 +8,7 @@ import { cn } from '@/lib/utils';
 import { Channel, ChannelType, MemberRole, Server } from '@prisma/client';
 
 // hooks
-import { useModal } from '@/hooks/use-modal-store';
+import { ModalType, useModal } from '@/hooks/use-modal-store';
 
 // components
 import { ActionTooltip } from '@/components/action-tooltip';
@@ -32,6 +32,15 @@ export const ServerChannel = ({ channel, server, role }: ServerChannelProps) => 
 
   const Icon = iconMap[channel.type];
 
+  const onClick = () => {
+    router.push(`/servers/${params?.serverId}/channels/${channel.id}`);
+  };
+
+  const onAction = (e: React.MouseEvent, action: ModalType) => {
+    e.stopPropagation();
+    onOpen(action, { channel, server });
+  };
+
   return (
     <button
       onClick={() => {}}
@@ -54,7 +63,7 @@ export const ServerChannel = ({ channel, server, role }: ServerChannelProps) => 
         <div className={'ml-auto flex items-center gap-x-2'}>
           <ActionTooltip label={'Edit'}>
             <Edit
-              onClick={() => onOpen('editChannel', { server, channel })}
+              onClick={e => onAction(e, 'editChannel')}
               className={
                 'hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition'
               }
@@ -62,9 +71,7 @@ export const ServerChannel = ({ channel, server, role }: ServerChannelProps) => 
           </ActionTooltip>
           <ActionTooltip label={'Delete'}>
             <Trash
-              onClick={() => {
-                onOpen('deleteChannel', { server, channel });
-              }}
+              onClick={e => onAction(e, 'deleteChannel')}
               className={
                 'hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition'
               }
