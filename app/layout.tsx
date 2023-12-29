428ccb7c780940fb5981027bfede32ecf06858d9
Su Hwan CHOI <hap_hazard@naver.com>
2023-12-29 19:27:50 +0900

    ✨feat: socket-provider 생성 및 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import { Open_Sans } from 'next/font/google';
 //component
 import { ThemeProvider } from '@/components/providers/theme-provider';
 import { ModalProvider } from '@/components/providers/modal-provider';
+import { SocketProvider } from '@/components/providers/socket-provider';
 
 // util
 import { cn } from '@/lib/utils';
@@ -35,8 +36,10 @@ export default function RootLayout({ children }: Children) {
             enableSystem={false}
             storageKey={'discord-theme'}
           >
-            <ModalProvider />
-            {children}
+            <SocketProvider>
+              <ModalProvider />
+              {children}
+            </SocketProvider>
           </ThemeProvider>
         </body>
       </html>
