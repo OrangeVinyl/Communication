c5ae2fdeda18dcdf26bbc74d3ffa4e17b4cd3e02
Su Hwan CHOI <hap_hazard@naver.com>
2023-12-30 00:50:51 +0900

    ✨feat: chat-input 컴포넌트 구현

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -7,6 +7,7 @@ import { db } from '@/lib/db';
 
 // components
 import { ChatHeader } from '@/components/chat/chat-header';
+import { ChatInput } from '@/components/chat/chat-input';
 
 interface ChannelIdPageProps {
   params: {
@@ -42,6 +43,16 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
   return (
     <div className={'bg-white dark:bg-[#313338] flex flex-col h-full'}>
       <ChatHeader name={channel.name} serverId={channel.serverId} type={'channel'} />
+      <div className={'flex-1'}>Future Message</div>
+      <ChatInput
+        name={channel.name}
+        type={'channel'}
+        apiUrl={'/api/socket/messages'}
+        query={{
+          channelId: channel.id,
+          serverId: channel.serverId,
+        }}
+      />
     </div>
   );
 };
