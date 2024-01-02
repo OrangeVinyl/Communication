8c380f87b996219a293c80436ab33108fd4dd690
Su Hwan CHOI <hap_hazard@naver.com>
2024-01-02 15:12:42 +0900

    ♻️refactor: 다른 멤버가 보낸 메세지를 클릭 시, 해당 멤버와의 채팅으로 이동 하는 기능 구현

--- components/chat/chat-item.tsx
+++ components/chat/chat-item.tsx
@@ -2,6 +2,7 @@
 
 import Image from 'next/image';
 import { useEffect, useState } from 'react';
+import { useRouter, useParams } from 'next/navigation';
 import * as z from 'zod';
 import axios from 'axios';
 import qs from 'query-string';
@@ -58,6 +59,8 @@ export const ChatItem = ({
   socketUrl,
   socketQuery,
 }: ChatItemProps) => {
+  const params = useParams();
+  const router = useRouter();
   const [isEditing, setIsEditing] = useState(false);
   const { onOpen } = useModal();
 
@@ -100,6 +103,14 @@ export const ChatItem = ({
     }
   };
 
+  const onMemberClick = () => {
+    if (member.id === currentMember.id) {
+      return;
+    }
+
+    router.push(`/servers/${params?.serverId}/conversations/${member.id}`);
+  };
+
   const isLoading = form.formState.isSubmitting;
   const isAdmin = currentMember.role === MemberRole.ADMIN;
   const isModerator = currentMember.role === MemberRole.MODERATOR;
@@ -113,13 +124,16 @@ export const ChatItem = ({
   return (
     <div className={'relative group flex items-center hover:bg-black/5 p-4 transition w-full'}>
       <div className={'group flex gap-x-2 items-start w-full'}>
-        <div className={'cursor-pointer hover:drop-shadow-md transition'}>
+        <div onClick={onMemberClick} className={'cursor-pointer hover:drop-shadow-md transition'}>
           <UserAvatar src={member.profile.imageUrl} />
         </div>
         <div className={'flex flex-col w-full'}>
           <div className={'flex items-center gap-x-2'}>
             <div className={'flex items-center'}>
-              <p className={'font-semibold text-sm hover:underline cursor-pointer'}>
+              <p
+                onClick={onMemberClick}
+                className={'font-semibold text-sm hover:underline cursor-pointer'}
+              >
                 {member.profile.name}
               </p>
               <ActionTooltip label={member.role}>{roleIconMap[member.role]}</ActionTooltip>
